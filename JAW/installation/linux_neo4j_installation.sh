c76fcfe87a12e734507424f108103a26ccfdebef
Jan Niklas Drescher <jan.drescher@tu-braunschweig.de>
2025-09-16 13:16:17 +0200

    update neo4j installation script #3

--- JAW/installation/linux_neo4j_installation.sh
+++ JAW/installation/linux_neo4j_installation.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # neo4j version family
-# see https://debian.neo4j.com/ for possible choices 
+# see https://debian.neo4j.com/ for possible choices
 VERSION='3.5'
 
-wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
-echo 'deb https://debian.neo4j.com stable' $VERSION | sudo tee /etc/apt/sources.list.d/neo4j.list
+wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/neotechnology.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/neotechnology.gpg] https://debian.neo4j.com stable' $VERSION | sudo tee -a /etc/apt/sources.list.d/neo4j.list
 sudo apt-get update
 sudo apt-get install neo4j
